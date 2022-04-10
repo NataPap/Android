79e22c47a72e93cdb905a3e5c1223097d362da4d
NataPap <papushynanataliya@gmail.com>
2022-04-10 18:17:00 +0300

    Added cards and upload photos

--- Android/app/src/main/java/com/example/shop/MainActivity.java
+++ Android/app/src/main/java/com/example/shop/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.shop.dto.CreateProductDTO;
+import com.example.shop.dto.CreateProductResultDTO;
+import com.example.shop.dto.ValidationCreateProductDTO;
 import com.example.shop.network.ProductService;
+import com.google.gson.Gson;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,8 +35,11 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextName;
     EditText editTextPrice;
     EditText editTextDescription;
-    EditText editTextImage;
 
+   // constant to compare
+    // the activity result code
+    int SELECT_PICTURE = 200;
+    String sImage="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +48,77 @@ public class MainActivity extends AppCompatActivity {
         editTextName = findViewById(R.id.editTextName);
         editTextPrice = findViewById(R.id.editTextPrice);
         editTextDescription = findViewById(R.id.editTextDescription);
-        editTextImage = findViewById(R.id.editTextImage);
+
+    }
+    public void handleSelectImageClick(View view) {
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri uri = data.getData();
+                // update the preview image in the layout
+                //IVPreviewImage.setImageURI(uri);
+                Bitmap bitmap= null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // initialize byte stream
+                ByteArrayOutputStream stream=new ByteArrayOutputStream();
+                // compress Bitmap
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+                // Initialize byte array
+                byte[] bytes=stream.toByteArray();
+                // get base64 encoded string
+                sImage= Base64.encodeToString(bytes,Base64.DEFAULT);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.m_zakuska:
+                try {
+                    intent = new Intent(MainActivity.this, ProductsActivity.class);
+                    startActivity(intent);
+                }
+                catch(Exception ex) {
+                    System.out.println("Problem "+ ex.getMessage());
+                }
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     public void handleClick(View view)
@@ -40,20 +127,33 @@ public class MainActivity extends AppCompatActivity {
                 editTextName.getText().toString(),
                 editTextPrice.getText().toString(),
                 editTextDescription.getText().toString(),
-                editTextImage.getText().toString()
+                sImage
+
         );
         ProductService
                 .getInstance()
                 .jsonApi()
                 .create(dto)
-                .enqueue(new Callback<Void>() {
+                .enqueue(new Callback<CreateProductResultDTO>() {
                     @Override
-                    public void onResponse(Call<Void> call, Response<Void> response) {
+                    public void onResponse(Call<CreateProductResultDTO> call, Response<CreateProductResultDTO> response) {
+                        if(response.isSuccessful()){
+                            CreateProductResultDTO reslt=response.body();
+                        }
+                        else {
+                            try {
+                               String json = response.errorBody().string();
+                                Gson gson = new Gson();
+                                ValidationCreateProductDTO serverError = gson.fromJson(json,
+                                        ValidationCreateProductDTO.class);
+                            }catch (Exception ex) {
 
+                            }
+                        }
                     }
 
                     @Override
-                    public void onFailure(Call<Void> call, Throwable t) {
+                    public void onFailure(Call<CreateProductResultDTO> call, Throwable t) {
 
                     }
                 });
